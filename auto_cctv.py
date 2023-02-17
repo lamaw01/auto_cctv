4eb603446d4fd83769bd048993a7773498982ac5
lamaw01 <Administrator@2J3.uc-1.domain>
2023-02-17 16:53:44 +0800

    added logs but not complete

--- auto_cctv.py
+++ auto_cctv.py
@@ -4,6 +4,8 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+import os
+from time import ctime
 
 #web elements
 login_button_element = '//*[@id="login"]/table/tbody/tr/td[2]/div/div[5]/button'
@@ -12,13 +14,22 @@ maintenance_element = '//*[@id="menu"]/div/div[2]/div[3]/span'
 reboot_button_element = '//*[@id="maintainUpgrade"]/div[1]/div[2]/span[1]/button'
 reboot_ok_button_element = '//*[@id="config"]/div[1]/div/table/tbody/tr[2]/td[2]/div/table/tbody/tr[3]/td/div/button[1]'
 camera_management_element = '//*[@id="menu"]/div/div[2]/div[5]/span'
-camera_status_element = '//*[@id="tableDigitalChannels"]/div/div[2]/div[1]/span[8]'
+
+#base row
 table_row = '//*[@id="tableDigitalChannels"]/div/div[2]/div'
-table_row_ip = '//*[@id="tableDigitalChannels"]/div/div[2]/div[2]/span[4]'
+#/span[3] -> camera name
+camera_name_element = '//*[@id="tableDigitalChannels"]/div/div[2]/div[1]/span[3]'
+#/span[4] -> camera ip
+camera_ip_element = '//*[@id="tableDigitalChannels"]/div/div[2]/div[1]/span[4]'
+#/span[8] -> camera status
+camera_status_element = '//*[@id="tableDigitalChannels"]/div/div[2]/div[1]/span[8]'
+
+
+global timeout
+timeout = 5
 
 #open nvr viewer
 def open_viewer():
-
    #credentials of nvr
    nvr_ip = 'http://172.21.0.198/'
    nvr_username = 'admin'
@@ -42,7 +53,7 @@ def open_viewer():
 
       wait_for_element_load(camera_management_element,nvr_driver)
       nvr_driver.find_element(By.XPATH,camera_management_element).click()
-      time.sleep(5)
+      time.sleep(timeout)
 
       #run 24/7
       while True:
@@ -60,17 +71,22 @@ def cycle_table(driver):
    is_tail = False
    while is_tail is False:
       try:
+         #click row
          driver.find_element(By.XPATH,table_row + str([camera_count+1])).click()
          camera_count = camera_count + 1
          print('camera count',camera_count)
          
-         camera_status = driver.find_element(By.XPATH,camera_status_element).text
+         #get name and status of current row
+         camera_name = driver.find_element(By.XPATH,table_row + str([camera_count]) + '/span[3]').text
+         camera_status = driver.find_element(By.XPATH,table_row + str([camera_count]) + '/span[8]').text
          
          #if offline, get ip and reboot
          if camera_status != 'Online':
             #select ip element and reboot
-            ip_selected = driver.find_element(By.XPATH,table_row+ str([camera_count])  + '/span[4]').text
+            ip_selected = driver.find_element(By.XPATH,table_row + str([camera_count])  + '/span[4]').text
             ip_link = 'http://' + ip_selected
+
+            write_logs(camera_name)
             reboot(ip_link)
 
       except Exception as e:
@@ -94,9 +110,9 @@ def reboot(ip):
       driver.get(ip)
 
       wait_for_element_load(login_button_element,driver)
-      
       login(driver)
-      time.sleep(5)
+      time.sleep(timeout)
+
       wait_for_element_load(config_element,driver)
       driver.find_element(By.XPATH, config_element).click()
 
@@ -107,8 +123,9 @@ def reboot(ip):
       driver.find_element(By.XPATH,reboot_button_element).click()
 
       wait_for_element_load(reboot_ok_button_element,driver)
-      #driver.find_element(By.XPATH,reboot_ok_button_element).click()
-      time.sleep(5)
+      driver.find_element(By.XPATH,reboot_ok_button_element).click()
+   
+      time.sleep(timeout)
       
    except Exception as e:
       print('reboot camera error',e)
@@ -117,6 +134,17 @@ def reboot(ip):
       #close current browser
       driver.close()
 
+#write logs
+def write_logs(cam_name):
+   is_file_exist = os.path.isfile('logs.txt')
+   if is_file_exist:
+      with open('logs.txt', 'a') as f:
+         f.write('\n' + cam_name + ' -> ' + ctime())
+   else:
+      with open('logs.txt', 'w') as f:
+         f.write('\n' + cam_name + ' -> ' + ctime())
+
+
 def login(driver):
    #credentials of selected ip cam
    username = 'admin'
@@ -124,24 +152,24 @@ def login(driver):
    password2 = 'scores135792468'
 
    pre_login_url = driver.current_url
-
+   #login using first password
    driver.find_element(By.ID,'username').send_keys(username)
-   driver.find_element(By.ID,'password').send_keys(password2)
+   driver.find_element(By.ID,'password').send_keys(password1)
    driver.find_element(By.XPATH,login_button_element).click()
    
-   time.sleep(5)
+   time.sleep(timeout)
    post_login_url = driver.current_url
    #check if password is error
    if pre_login_url == post_login_url:
       driver.find_element(By.ID,'username').send_keys(username)
-      driver.find_element(By.ID,'password').send_keys(password1)
+      driver.find_element(By.ID,'password').send_keys(password2)
       driver.find_element(By.XPATH,login_button_element).click()
 
 #function to ensure web element loaded
 def wait_for_element_load(element, driver):
    element_wait_load = EC.presence_of_element_located((By.XPATH, element))
    WebDriverWait(driver, 10).until(element_wait_load)
-   time.sleep(5)
+   time.sleep(timeout)
 
 open_viewer()
 #reboot('http://192.168.69.107')
