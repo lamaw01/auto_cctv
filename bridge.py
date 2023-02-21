b73d40034902090cc0c549750b79f1bc20f92ec6
lamaw01 <Administrator@2J3.uc-1.domain>
2023-02-21 14:56:45 +0800

    dual script ok

--- bridge.py
+++ bridge.py
@@ -19,55 +19,55 @@ reboot_button_element = '//*[@id="maintainUpgrade"]/div[1]/div[2]/span[1]/button
 reboot_ok_button_element = '//*[@id="config"]/div[1]/div/table/tbody/tr[2]/td[2]/div/table/tbody/tr[3]/td/div/button[1]'
 
 #open ip cam page
-def reboot(ip,name):
-    try:
-        chrome_options = Options()
-        #chrome_options.add_argument("--incognito")
-        chrome_options.add_experimental_option("detach", True)
-        global cam_driver
-        cam_driver = webdriver.Chrome(options=chrome_options)
-        #set position and size
-        cam_driver.set_window_size(1280, 720)
-        #cam_driver.set_window_position(350, 20, windowHandle='current')
-        cam_driver.get('http://'+ip)
-        success = True
-        #credentials of selected ip cam
-        username = 'admin'
-        password1 = '123456@ad'
-        password2 = 'scores135792468'
-        time.sleep(timeout)
-        wait_for_element_load(login_button_element,cam_driver)
-        pre_login_url = cam_driver.current_url
-        #login using first password
-        cam_driver.find_element(By.ID,'username').send_keys(username)
-        cam_driver.find_element(By.ID,'password').send_keys(password1)
-        cam_driver.find_element(By.XPATH,login_button_element).click()
-        time.sleep(timeout)
-        post_login_url = cam_driver.current_url
-        #check if password is error
-        if pre_login_url == post_login_url:
-            cam_driver.find_element(By.ID,'username').send_keys(username)
-            cam_driver.find_element(By.ID,'password').send_keys(password2)
-            cam_driver.find_element(By.XPATH,login_button_element).click()
-            time.sleep(timeout)
-            wait_for_element_load(cam_config_element,cam_driver)
-            cam_driver.find_element(By.XPATH, cam_config_element).click()
-            wait_for_element_load(maintenance_element,cam_driver)
-            cam_driver.find_element(By.XPATH,maintenance_element).click()
-            wait_for_element_load(reboot_button_element,cam_driver)
-            cam_driver.find_element(By.XPATH,reboot_button_element).click()
-            wait_for_element_load(reboot_ok_button_element,cam_driver)
-            cam_driver.find_element(By.XPATH,reboot_ok_button_element).click()
-            time.sleep(timeout)
-    except Exception as e:
-        print('reboot camera error',e)
-        success = False
-    finally:
-        #close current browser
-        cam_driver.close()
-        #write log
-        write_excel(ip,name,success,198)
-        print('is rebooted?..'+name,success)
+def reboot(ip,name,x,y):
+   print('accessing...',name)
+   try:
+      chrome_options = Options()
+      #chrome_options.add_argument("--incognito")
+      chrome_options.add_experimental_option("detach", True)
+      cam_driver = webdriver.Chrome(options=chrome_options)
+      #set position and size
+      cam_driver.set_window_size(1280, 720)
+      cam_driver.set_window_position(x, y, windowHandle='current')
+      cam_driver.get('http://'+ip)
+      success = True
+      #credentials of selected ip cam
+      username = 'admin'
+      password1 = '123456@ad'
+      password2 = 'scores135792468'
+      time.sleep(timeout)
+      wait_for_element_load(login_button_element,cam_driver)
+      pre_login_url = cam_driver.current_url
+      #login using first password
+      cam_driver.find_element(By.ID,'username').send_keys(username)
+      cam_driver.find_element(By.ID,'password').send_keys(password1)
+      cam_driver.find_element(By.XPATH,login_button_element).click()
+      time.sleep(timeout)
+      post_login_url = cam_driver.current_url
+      #check if password is error
+      if pre_login_url == post_login_url:
+         cam_driver.find_element(By.ID,'username').send_keys(username)
+         cam_driver.find_element(By.ID,'password').send_keys(password2)
+         cam_driver.find_element(By.XPATH,login_button_element).click()
+         time.sleep(timeout)
+         wait_for_element_load(cam_config_element,cam_driver)
+         cam_driver.find_element(By.XPATH, cam_config_element).click()
+         wait_for_element_load(maintenance_element,cam_driver)
+         cam_driver.find_element(By.XPATH,maintenance_element).click()
+         wait_for_element_load(reboot_button_element,cam_driver)
+         cam_driver.find_element(By.XPATH,reboot_button_element).click()
+         wait_for_element_load(reboot_ok_button_element,cam_driver)
+         cam_driver.find_element(By.XPATH,reboot_ok_button_element).click()
+         time.sleep(timeout)
+   except Exception as e:
+      print('reboot camera error',e)
+      success = False
+   finally:
+      #close current browser
+      cam_driver.close()
+      #write log
+      write_excel(ip,name,success,198)
+      print('is rebooted?..'+name,success)
 
 #function to ensure web element loaded
 def wait_for_element_load(element, driver):
